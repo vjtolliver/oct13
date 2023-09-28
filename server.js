db0920d527f34f1a98aebd84146ff73eadb63c0c
vjtolliver <vj.tolliver@gmail.com>
2023-09-28 17:32:04 -0500

    port is 3001

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 app.use("/css", express.static(path.join(__dirname, "/public/css")));
 
 
