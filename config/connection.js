d33befc78aee5dc5b720781b88d9f0e949d2636b
vjtolliver <vj.tolliver@gmail.com>
2023-09-29 18:02:54 -0500

    added else statement back

--- config/connection.js
+++ config/connection.js
@@ -5,20 +5,18 @@ let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
-} 
-
-// else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//      {
-//       host:   '127.0.0.1',
-//       dialect: 'mysql',
-//       port: 3001
+} else {
+   sequelize = new Sequelize(
+     process.env.DB_NAME,
+     process.env.DB_USER,
+     process.env.DB_PASSWORD,
+      {
+       host:   '127.0.0.1',
+       dialect: 'mysql',
+       port: 3001
        
-//      }
-//   );
-// }
+      }
+   );
+ }
 
 module.exports = sequelize;
