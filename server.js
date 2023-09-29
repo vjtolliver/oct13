ea0dbb9d439769e111ad9e27f95496162f4efc0a
vjtolliver <vj.tolliver@gmail.com>
2023-09-29 17:04:57 -0500

    back to sync

--- server.js
+++ server.js
@@ -44,6 +44,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-sequelize.authenticate({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening to RSVP and', PORT));
 });
