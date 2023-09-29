bf9c90f9dd211b9da3595be26248e8f4e4b9d024
vjtolliver <vj.tolliver@gmail.com>
2023-09-29 17:23:04 -0500

    switched localhost to number

--- config/connection.js
+++ config/connection.js
@@ -11,8 +11,9 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
      {
-       host: 'localhost',
-       dialect: 'mysql'
+      host:   '127.0.0.1',
+      dialect: 'mysql',
+      port: 3001
        
      }
   );
