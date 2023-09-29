39bb64fb5734837e236ebdaeac66e0dd0cf5f2ce
vjtolliver <vj.tolliver@gmail.com>
2023-09-29 16:59:28 -0500

    switched start script to index

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-//app.use("/css", express.static(path.join(__dirname, "/public/css")));
+app.use("/css", express.static(path.join(__dirname, "/public/css")));
 
 
 
