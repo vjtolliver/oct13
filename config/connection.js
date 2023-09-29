091f16bc743bc1224803051f9a1ec9dd4d3759b7
vjtolliver <vj.tolliver@gmail.com>
2023-09-29 17:14:42 -0500

    removed hardcoded ports

--- config/connection.js
+++ config/connection.js
@@ -12,8 +12,8 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_PASSWORD,
      {
        host: 'localhost',
-       dialect: 'mysql',
-       port: 3001
+       dialect: 'mysql'
+       
      }
   );
 }
